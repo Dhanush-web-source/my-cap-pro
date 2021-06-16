89013cecc4efc75d5516d76b98e0f21e0cc123a3
Dhanush-web-source <85722469+Dhanush-web-source@users.noreply.github.com>
2021-06-16 19:41:44 +0530

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,4 @@
+#tTO FIND THE AREA OF THE CIRCLE
+r=float(input("Enter The Radius Of The Circle:"))
+area=3.14*r*r
+print("The Area Of The Circle With Radius",r,"is",area)
---
+++ extension.py
@@ -0,0 +1,3 @@
+Filename=input("enter the filename:")
+f_extension=Filename.split(".")
+print("the extension of the file is:"+repr(f_extension[-1]))
